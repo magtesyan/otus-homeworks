3e9d44b7fe195490b86998bb91edf0b28fd29a44
Mikael Magtesyan <magtesyan@inbox.ru>
2024-12-02 21:13:42 +0300

    Add starting program

---
+++ homework/Commands.cs
@@ -0,0 +1,29 @@
+﻿namespace Homework
+{
+    public class Commands
+    {
+        public const string Start = "/start";
+        public const string Info = "/info";
+        public const string Help = "/help";
+        public const string Echo = "/echo";
+        public const string Exit = "/exit";
+
+        public static string[] availableWithoutNameCommands = { Start, Info, Help, Exit };
+        public static string[] availableWithNameCommands = { Info, Help, Echo, Exit };
+
+        public static void PrintAvailableCommands(bool hasName)
+        {
+            string[] availableCommands = hasName ? availableWithNameCommands : availableWithoutNameCommands;
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine(string.Join(" ", availableCommands));
+            Console.ResetColor();
+        }
+
+        public static void PrintNotAvailableCommandError()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Такой команды нет в списке доступных!");
+            Console.ResetColor();
+        }
+    }
+}
---
+++ homework/Program.cs
@@ -0,0 +1,89 @@
+﻿
+namespace Homework
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string enteredCommand = "";
+            string name = "";
+
+            PrintGreeting(name, []);
+            while (enteredCommand != Commands.Exit)
+            {
+                enteredCommand = Console.ReadLine() ?? "";
+
+                if (name != "" && enteredCommand.StartsWith(Commands.Echo))
+                {
+                    Console.WriteLine(enteredCommand.Substring(Commands.Echo.Length + 1));
+                    PrintGreeting(name, []);
+                    continue;
+                }
+
+                switch (enteredCommand)
+                {
+                    case Commands.Start:
+                        if (name == "")
+                        {
+                            Console.Write("\nВведите ваше имя: ");
+                            name = Console.ReadLine() ?? "";
+                        }
+                        else
+                        {
+                            Console.WriteLine("\nПрограмма уже запущена");
+                        }
+                        PrintGreeting(name, []);
+                        break;
+                    case Commands.Info:
+                        PrintInfo();
+                        PrintGreeting(name, []);
+                        break;
+                    case Commands.Help:
+                        PrintHelp();
+                        PrintGreeting(name, []);
+                        break;
+                    case Commands.Exit:
+                        break;
+                    default:
+                        Commands.PrintNotAvailableCommandError();
+                        break;
+                }
+            }
+        }
+
+        private static void PrintGreeting(string name, string[] commands)
+        {
+            bool hasName = name != "";
+            string nameOrHello = hasName ? $"{name}" : "Привет";
+            Console.WriteLine($"{nameOrHello}, введите команду из списка доступных:");
+            Commands.PrintAvailableCommands(hasName);
+        }
+
+
+
+        private static void PrintInfo()
+        {
+            Console.WriteLine("v.1.0, Release: 18.11.2024\n");
+        }
+
+        private static void PrintHelp()
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.Write("/start");
+            Console.ResetColor();
+            Console.WriteLine(" - Начало программы, нужно будет ввести свое имя");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.Write("/info");
+            Console.ResetColor();
+            Console.WriteLine(" - предоставляет информацию о версии программы и дате её создания");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.Write("/echo");
+            Console.ResetColor();
+            Console.WriteLine(" - При вводе этой команды с аргументом, программа возвращает введенный текст");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.Write("/exit");
+            Console.ResetColor();
+            Console.WriteLine(" - Выход\n");
+        }
+    }
+}
