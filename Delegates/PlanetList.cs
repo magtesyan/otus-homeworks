2393229e62b67791fe333653db98e127e757a9f6
Mikael Magtesyan <magtesyan@inbox.ru>
2025-01-02 19:31:25 +0300

    Add delegate homeworks

---
+++ Delegates/PlanetList.cs
@@ -0,0 +1,30 @@
+﻿namespace Delegates
+{
+    public class PlanetList
+    {
+        private readonly List<Planet> Planets = [];
+
+        public delegate string PlanetValidator(string name);
+
+        public PlanetList()
+        {
+            Planet Venus = new("Венера", 2, 38052, null);
+            Planet Earth = new("Земля", 3, 40075696, Venus);
+            Planet Mars = new("Марс", 4, 21344, Earth);
+            Planets.AddRange([Venus, Earth, Mars]);
+        }
+
+        public (int, long, string) GetPlanet(string name, PlanetValidator validator)
+        {
+            string validationError = validator(name);
+            if (!string.IsNullOrEmpty(validationError)) return (0, 0, validationError);
+
+            {
+                
+            }
+            Planet? foundPlanet = Planets.Find((planet) => planet.Name == name);
+
+            if (foundPlanet == null) return (0, 0, "Не удалось найти планету");
+            return (foundPlanet.Index, foundPlanet.Equator, "");
+    }
+}}
---
+++ Delegates/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace Delegates
+{
+    internal class Program
+    {
+        private static readonly PlanetList Planets = new();
+        private static int TryCount;
+
+        static void Main()
+        {
+            static void PrintFoundPlanet(string name)
+            {
+                TryCount++;
+                (int index, long equator, string error) = Planets.GetPlanet(name, (string name) =>
+                {
+                    if (TryCount == 3)
+                    {
+                        TryCount = 0;
+                        return "Вы спрашиваете слишком часто";
+                    }
+
+                    if (name == "Лимония")
+                        return "Это запретная планета";
+
+                    return "";
+                });
+
+                if (!string.IsNullOrEmpty(error))
+                {
+                    Console.WriteLine($"{error}\n");
+                    return;
+                }
+
+                Console.WriteLine($"Название планеты: {name}\n" +
+                    $"Порядковый номер от солнца: {index}\n" +
+                    $"Длина экватора: {equator}\n");
+            }
+
+            PrintFoundPlanet("Земля");
+            PrintFoundPlanet("Лимония");
+            PrintFoundPlanet("Марс");
+            PrintFoundPlanet("Земля");
+            PrintFoundPlanet("Не Земля");
+        }
+    }
+}
