34fc4a1bc2ebf55eaadea8c2a15b29f0b4b84146
Mikael Magtesyan <magtesyan@inbox.ru>
2025-02-14 22:54:04 +0300

    Add delegate homework

---
+++ DelegatBot/DelegatBot/Program.cs
@@ -0,0 +1,59 @@
+﻿using Telegram.Bot.Polling;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot;
+using DelegatBot;
+using dotenv.net;
+
+static void Start(string message)
+{
+    Console.WriteLine($"Началась обработка сообщения {message}");
+}
+
+static void Complete(string message)
+{
+    Console.WriteLine($"Закончилась обработка сообщения {message}");
+}
+
+DotEnv.Load();
+var token = DotEnv.Read()["TOKEN"];
+var botClient = new TelegramBotClient("8120356962:AAFrMWFTn0bU8TRZSYtvZjPOvkahIjiE0c8");
+var cancellationTokenSource = new CancellationTokenSource();
+
+var receiverOptions = new ReceiverOptions
+{
+    AllowedUpdates = [UpdateType.Message],
+    DropPendingUpdates = true
+};
+var handler = new UpdateHandler();
+
+handler.OnHandleUpdateStarted += Start;
+handler.OnHandleUpdateCompleted += Complete;
+
+botClient.StartReceiving(handler, receiverOptions);
+
+var me = await botClient.GetMe();
+Console.WriteLine($"{me.FirstName} запущен!");
+
+Console.WriteLine("Нажмите клавишу A для выхода");
+while (true)
+{
+    ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+    Console.WriteLine(keyInfo.KeyChar);
+    if (keyInfo.KeyChar == 'A')
+    {
+        try
+        {
+            if (handler != null)
+            {
+                handler.OnHandleUpdateStarted -= Start;
+                handler.OnHandleUpdateCompleted -= Complete;
+                cancellationTokenSource.Cancel();
+            }
+        }
+        finally { handler = null; }
+        break;
+    } else
+    {
+        Console.WriteLine($"{me?.ToString()}");
+    }
+}
---
+++ DelegatBot/DelegatBot/UpdateHandler.cs
@@ -0,0 +1,60 @@
+﻿using Telegram.Bot.Polling;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types;
+using Telegram.Bot;
+using System.Net.Http.Json;
+using System.Threading;
+
+namespace DelegatBot
+{
+    public class UpdateHandler : IUpdateHandler
+    {
+        public delegate void MessageHandler(string message);
+        public event MessageHandler? OnHandleUpdateStarted;
+        public event MessageHandler? OnHandleUpdateCompleted;
+        public record CatFactDto(string fact, int length);
+        public Task HandleUpdateAsync(ITelegramBotClient client, Update update, CancellationToken cancellationToken)
+        {
+            if (update.Type == UpdateType.Message && update.Message != null)
+            {
+                var message = update.Message;
+                OnHandleUpdateStarted?.Invoke(message.Text ?? "");
+
+                if (message.Text != null && message.Text.StartsWith('/'))
+                {
+                    switch (message.Text.Split(' ')[0])
+                    {
+                        case "/cat":
+                            var fact = GetRandomFact().Result;
+                            return SendMessage(client, message.Chat.Id, fact);
+                    }
+                }
+                else
+                {
+                    OnHandleUpdateCompleted?.Invoke(message.Text ?? "");
+                    return SendMessage(client, message.Chat.Id, $"Сообщение успешно принято");
+                }
+            }
+
+            return Task.CompletedTask;
+        }
+
+        private static async Task SendMessage(ITelegramBotClient client, long chatId, string text)
+        {
+            await client.SendMessage(chatId, text);
+        }
+
+        public Task HandleErrorAsync(ITelegramBotClient botClient, Exception exception, HandleErrorSource source, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static async Task<string> GetRandomFact()
+        {
+            var cts = new CancellationTokenSource();
+            using var client = new HttpClient();
+            var catFact = await client.GetFromJsonAsync<CatFactDto>("https://catfact.ninja/fact", cts.Token);
+            return catFact?.fact ?? "";
+        }
+    }
+}
